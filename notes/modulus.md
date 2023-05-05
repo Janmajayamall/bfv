b2fb73ef840db22ffbdaa65f58b657c9c81cf6e3
Janmajaya Mall <janmajaya.choithram@gmail.com>
2023-05-05 13:19:27 +0530

    add shoup multiplication

--- notes/modulus.md
+++ notes/modulus.md
@@ -5,7 +5,7 @@ $y = x - (q \times m), q = \lfloor \frac{x}{m} \rfloor $
 
 However computing $q$ requires integer division which is expensive in computers. Barrett modulus reduction is a way to avoid integer division. Instead it uses pre-computed inverse of $m$ multiplied by some factor and estimates $q$ using only shifts and multiplication.
 
-Algorithm 2 of https://homes.esat.kuleuven.be/~fvercaut/papers/bar_mont.pdf specifies generalised barrett modulus reduction for integers.
+Algorithm 2 of https://homes.esat.kuleuven.be/~fvercaut/papers/bar_mont.pdf specifies generalised barrett modulus reduction for integers and implementation refers to https://github.com/openfheorg/openfhe-development/blob/c48c41cf7893feb94f09c7d95284a36145ec0d5e/src/core/include/math/hal/intnat/ubintnat.h#L1417.
 
 ### $<64 + \gamma$ bits by $<64$ bits modulus reduction
 
@@ -14,12 +14,27 @@ Algorithm 2 of https://homes.esat.kuleuven.be/~fvercaut/papers/bar_mont.pdf spec
 3. $ \alpha = n + 3 $ and $ \beta = -2 $. Generally $\alpha \ge  \gamma + 1$ where $\gamma + n \ge log_{2} x$ and $x$ is dividend in $\lfloor \frac{x}{m} \rfloor$.
 4. $\mu = \frac{2^{\alpha + n}}{m} = \frac{2^{2n + 3}}{m} $
 5. $\mu$ is stored in 64 bits. Since $\log_2mu = (2n+3) - n = n+3$, $\mu$ will only fit in 64 bits if $n < 61$. Hence, implementation will given incorrect result for $n \ge 61$.
-6. $\alpha = n+3$ and $\beta = -2$ play an important role in estimation of $q = \lfloor \frac {x}{m} \rfloor$. I have observed with set $\alpha \& \beta$ values that as long as $log_2x < 2n$ estimation of $q$ is accurate such that you do not pay for correction cost.
+6. $\alpha = n+3$ and $\beta = -2$ play an important role in estimation of $q = \lfloor \frac {x}{m} \rfloor$. I have observed with set $\alpha$ and $\b$eta$ values that as long as $log_2x < 2n$ estimation of $q$ is accurate such that you do not pay for correction cost.
 7. Generally if $log_2x \ge 2n$ the result will still be correct however you will have to pay for a correction cost. Although, loop iteration is limited to 1-2 times.
 8. $log_2x$ cannot be arbitrarily big due to 128 bit limitation of implementation. To be specific, when we multiple $ab >> (n-2)$ by $\mu$ we must satisfy following in-equality to prevent overflow: $log_2x - (n-2) + \log_2mu < 128$. Since $log_2mu = 2n + 3 - n = n + 3$, we can write in-equality as $log_2x + 5 < 128$. Hence, the result should be correct as long as $log_2x < 123$.
 
 ### 128 bits by 64 bits modulus reduction
 
 1. We want to reduce a value that is `2n` bits by modulus that is `n` bits. The implementation follows Algorithm 14.42 of https://cacr.uwaterloo.ca/hac/about/chap14.pdf and https://github.com/openfheorg/openfhe-development/blob/055c89778d3d0dad00479150a053124137f4c3ca/src/core/include/utils/utilities-int.h#L59.
+
+### Shoup Multiplication
+
+First implemented in NTL by Victor Shoup and more details can be found in lines 5-7 of algorithm 2 in [Faster arithmetic for number theory transforms](https://arxiv.org/pdf/1205.2926.pdf).
+
+To calculate $xy \mod m$ we need to calculate $xy - q \times m$ where $q = \lfloor \frac {xy}{m} \rfloor$. In shoup multiplication we pre-compute x' such that $x' \times y$ gives accurate estimation of $q$.
+
+$\beta$ is usually set equivalent to word-size. For 64-bit $\beta = 2^{64}$
+$$x' = \frac {(x << \beta)}{m}$$
+
+To calculate $(x \times y) \mod m$:
+
+1. $q = \frac {x' \times y} { \beta}$ (Notice we only need higher bits of $x'y$)
+2. $r = (x \times y ) - (q \times m)$
+3. if $r \ge m$ then $r-=m$
 
 ### When to use which?
--- src/modulus.rs
+++ src/modulus.rs
@@ -10,9 +10,7 @@ impl Modulus {
     fn new(modulus: u64) -> Modulus {
         // mu = 2^(2n+3) / modulus
         let n = 64 - (modulus.leading_zeros() as u64);
-        dbg!(n);
         let mu = (1u128 << (2 * n + 3)) / (modulus as u128);
-        dbg!(mu);
         Modulus {
             mu_hi: 2,
             mu_lo: 2,
@@ -22,18 +20,101 @@ impl Modulus {
         }
     }
 
-    /// Barrett modulur multiplication. Assumes that a and b are < modulus
+    /// Computes shoup representation
     ///
-    /// If either of a and b are greater than modulus then q*mu overflows.
-    fn mul_mod_fast(&self, a: u64, b: u64) -> u64 {
-        // let mut ab = a as u128 * b as u128;
-        let mut ab = (1u128 << 122) - 1;
+    /// a should be smaller than modulus
+    fn compute_shoup(&self, a: u64) -> u64 {
+        debug_assert!(a < self.modulus);
+        (((a as u128) << 64) / self.modulus) as u64
+    }
 
-        {
-            dbg!(128 - ab.leading_zeros());
-            dbg!(ab % (self.modulus as u128));
+    /// Barrett modulus reduction of 64 bits value.
+    fn reduce(&self, a: u64) -> u64 {
+        let n = self.mod_bits;
+        let alpha = n + 3;
+        // beta = -2
+
+        let mut a = a as u128;
+        let mut q = a >> (n - 2);
+        q *= (self.mu as u128);
+        q >>= (alpha + 2);
+
+        a -= q * (self.modulus as u128);
+        let mut a = a as u64;
+
+        // correction
+        if a >= self.modulus {
+            a -= self.modulus;
+        }
+        a
+    }
+
+    /// Barrett modulus addition
+    fn add_mod(&self, mut a: u64, mut b: u64) -> u64 {
+        if a >= self.modulus {
+            a = self.reduce(a);
+        }
+        if b >= self.modulus {
+            b = self.reduce(b);
         }
 
+        a += b;
+        if a >= self.modulus {
+            a -= self.modulus;
+        }
+
+        a
+    }
+
+    /// Modulus addition
+    ///
+    /// Assumes both a and b are smaller than modulus
+    fn add_mod_fast(&self, a: u64, b: u64) -> u64 {
+        debug_assert!(a < self.modulus);
+        debug_assert!(b < self.modulus);
+
+        let mut c = a + b;
+        if c >= self.modulus {
+            c -= self.modulus;
+        }
+        c
+    }
+
+    /// Barret modulus subtraction
+    fn sub_mod(&self, mut a: u64, mut b: u64) -> u64 {
+        if a >= self.modulus {
+            a = self.reduce(a);
+        }
+        if b >= self.modulus {
+            b = self.reduce(b);
+        }
+
+        if a >= b {
+            a - b
+        } else {
+            (a + self.modulus) - b
+        }
+    }
+
+    /// Modulus subtraction
+    ///
+    /// Assumes both a and b < modulus
+    fn sub_mod_fast(&self, a: u64, b: u64) -> u64 {
+        debug_assert!(a < self.modulus);
+        debug_assert!(b < self.modulus);
+
+        if a >= b {
+            a - b
+        } else {
+            (a + self.modulus) - b
+        }
+    }
+
+    /// Barrett modulur multiplication. Assumes that a and b are < modulus.
+    ///
+    /// Refer to implementation notes for more details
+    fn mul_mod_fast(&self, a: u64, b: u64) -> u64 {
+        let mut ab = a as u128 * b as u128;
         let n = self.mod_bits;
         let alpha = n + 3;
         // beta = -2
@@ -50,9 +131,25 @@ impl Modulus {
             ab -= self.modulus;
         }
 
-        dbg!(ab);
-
         ab
+    }
+
+    /// Shoup modulur multiplication
+    ///
+    /// a and b should be smaller than modulus
+    fn mul_mod_shoup(&self, a: u64, b: u64, b_shoup: u64) -> u64 {
+        debug_assert!(self.compute_shoup(b) == b_shoup);
+        debug_assert!(a < self.modulus);
+        debug_assert!(b < self.modulus);
+
+        let q = (a as u128 * b_shoup as u128) >> 64;
+        let r = ((a as u128 * b as u128) - (q * self.modulus as u128)) as u64;
+
+        if r >= self.modulus {
+            r -= self.modulus
+        }
+
+        r
     }
 }
 
@@ -63,22 +160,18 @@ mod tests {
     use super::*;
     use crate::nb_theory::generate_prime;
 
+    // TODO: write perf for mul_mod_fast
     #[test]
     fn mul_mod_fast_works() {
-        let prime = generate_prime(61, 16, 1 << 61).unwrap();
+        let prime = generate_prime(60, 16, 1 << 60).unwrap();
         let modulus = Modulus::new(prime);
-
         let mut rng = thread_rng();
-        let a = rng.gen::<u64>();
-        let b = rng.gen::<u64>();
+        for _ in 0..1000 {
+            let a = rng.gen::<u64>() % prime;
+            let b = rng.gen::<u64>() % prime;
 
-        let p_bits = (128 - (a as u128 * b as u128).leading_zeros()) as u64;
-
-        // let mu_bits = (64 - (modulus.mu.leading_zeros())) as u64;
-        // let prod_bits = p_bits - (modulus.mod_bits - 2) + mu_bits;
-        // dbg!(p_bits, mu_bits, prod_bits);
-
-        let res = modulus.mul_mod_fast(a, b);
-        // dbg!(res, (a as u128 * b as u128) % (prime as u128));
+            let res = modulus.mul_mod_fast(a, b);
+            assert_eq!(res, ((a as u128 * b as u128) % (prime as u128)) as u64);
+        }
     }
 }
